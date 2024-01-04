a65e4c74bea289474e16f2e40ce216b2635a35cd
rajvimiracle <155509512+rajvimiracle@users.noreply.github.com>
2024-01-04 15:52:11 +0530

    init commit

---
+++ lib/condinational.dart
@@ -0,0 +1,30 @@
+main(){
+  var a = 500;
+  var b = 50;
+
+ // *jiyare pan work and condinational differant hoy tiyare
+  if(a>200 && b<10){
+    print('Block 1');
+  }else{
+    print('Block 2');
+  }
+
+
+//   *jiyare pan work and condinational sem hoy tiyare
+//   if(a>200 && b>100){
+// //cond 1  and cond 2 is true
+//   print("Block1");
+//
+//   }else if(a<100){
+// //cond 2 is true
+//     print("Block2");
+//   }else if (a == 80){
+// //cond 3 is true
+//     print("Block3");
+//   }else if(a == 500){
+//   //cond 4 is true
+//   print("Block 4");
+// //all counds are flase
+//     print("Block4 ELSE");
+//   }
+}
