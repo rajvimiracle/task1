e6547bdabdb5940f7adfde6ab4f87a4ac713a1f0
rajvimiracle <155509512+rajvimiracle@users.noreply.github.com>
2024-01-04 16:25:44 +0530

    init commit

---
+++ lib/loops.dart
@@ -0,0 +1,31 @@
+import 'dart:js_util';
+
+main(){
+
+ // * for loop
+ //  for(int a =1; a<=10; a++) {
+ //    print("Hello World");
+ //  }
+
+  // * do..while..
+  //
+  // int no  =100;
+  //
+  // do{
+  //   print("No is $no");
+  //
+  // }while(no<50);
+
+  // *while...do...
+
+  int no =10;
+  while(no <50){
+    print("No is $no");
+    no++;
+        // and
+    // no = no +1;
+    //     and
+    // no+ = 1;
+  }
+
+}
