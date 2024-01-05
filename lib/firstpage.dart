68d9a00c325ea62de67382d001b0e45e6a23ddb8
rajvimiracle <155509512+rajvimiracle@users.noreply.github.com>
2024-01-05 12:13:38 +0530

    init commit

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -14,7 +14,25 @@ class _FirstPageState extends State<FirstPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Image.asset("")
+            Image.asset('assets/images/h2.png',width: double.infinity,
+            ),
+            Positioned(child: Padding(
+              padding: const EdgeInsets.all(5.w),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.white,
+                ),
+                alignment: Alignment.bottomCenter,
+                height: 20.h,
+                width: 80.w,
+                child: Column(
+                  children: [
+                    Text("Book Your Next",),
+                  ],
+                ),
+              ),
+            ))
           ],
         ),
       ),
